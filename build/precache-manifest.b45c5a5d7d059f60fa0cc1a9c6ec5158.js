6fab5a719c257ca76182813b1485736765782544
Elio Struyf <info@estruyf.be>
2020-03-06 21:45:10 +0100

    Updated styling

---
+++ build/precache-manifest.b45c5a5d7d059f60fa0cc1a9c6ec5158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae04e0f8514a47de12eedef09fc4b58e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88a4feeef909b63cac1c",
+    "url": "/static/css/main.c2001dbb.chunk.css"
+  },
+  {
+    "revision": "15fd894bee7675f8339e",
+    "url": "/static/js/2.f5def501.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/static/js/2.f5def501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a4feeef909b63cac1c",
+    "url": "/static/js/main.65f3387d.chunk.js"
+  },
+  {
+    "revision": "7f9b62a7b0e8ba52c203",
+    "url": "/static/js/runtime-main.f0e643bc.js"
+  }
+]);
